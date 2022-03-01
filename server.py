c64ee8c80038eef40a0a3a9bfcf898635272e5a5
Benny Li <96bennyl@gmail.com>
2022-02-28 23:06:32 -0800

    implement client.py

--- server.py
+++ server.py
@@ -2,32 +2,60 @@ import socket
 import concurrent.futures
 
 PORT = 3000
-HOST = socket.gethostbyname(socket.gethostname())
+HOST = '127.0.0.1'
 MAX_CLIENTS = 5
+CHAT_NAME = "Benny's chatroom"
 clients = list()
+pool = concurrent.futures.ThreadPoolExecutor()
 
-def handle_client(conn, addr):
+def handle_client(conn_socket, addr):
     # ask for client name 
-    return
+    conn_socket.send(f"Welcome to {CHAT_NAME}! Please Enter your name: ".encode())
+    name = conn_socket.recv(16).decode()
+    
+
+    # terminate connection if max clients reached
+    if len(clients) == MAX_CLIENTS:
+        conn_socket.send(f"Sorry, {name}. Too many users in chat room... Ending Connection".encode())
+        conn_socket.close()
+        return
+    else:
+        clients.append(conn_socket)
+        conn_socket.send(f"Hello, {name}!".encode())
+
+        print(f"New client connected: {addr}")
+        print(f"Total clients: {len(clients)}")
+
+    while True:
+        msg = conn_socket.recv(1024).decode()
+        if not msg:
+            print(f"Client at {addr} disconnected")
+            conn_socket.close()
+            clients.remove(conn_socket)
+            break
+        else:
+            print(f"{name}: " + msg)
+            broadcast(conn_socket, f"{name}: " + msg)
+
+        
+def broadcast(conn_socket, msg):
+    for client in clients:
+        if client != conn_socket:
+            try:
+                client.send(msg.encode())
+            except: 
+                client.close()
+                remove(client)
 
 def main():
-    with socket(socket.AF_INET. socket.SOCK_STREAM) as server_socket:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, PORT))
         server_socket.listen()
         print(f"Server listening on {HOST} at port {PORT}")
         while True:
             conn_socket, addr = server_socket.accept()
-            
-            # terminate connection if max clients reached
-            if len(clients) == MAX_CLIENTS:
-                conn_socket.send("Too many users in chat room... Ending Connection".encode())
-                conn_socket.close()
-            else:
-                clients.append(conn_socket)
-                print(f"New client connected: {addr}")
 
-                with concurrent.futures.ThreadPoolExecutor() as executor:
-                    executor.submit(handle_client, conn_socket, addr)
+            pool.submit(handle_client, conn_socket, addr)
                 
 
 
